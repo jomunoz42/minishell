102631da8d697a1042b2e8c4872a13b6f537b860
Pedro Louzada <pedrolouzada17@gmail.com>
2025-11-11 17:46:43 +0000

    missing only in heredoc and ctrl + D

--- src/parse/signals.c
+++ src/parse/signals.c
@@ -5,16 +5,11 @@ void	handler(int sig)
 	t_map	*env;
 
 	env = get_map_addr(NULL);
-	if (sig == SIGINT)
-	{
-		write(1, "\n", 1);
-		rl_on_new_line();
-		rl_replace_line("", 1);
-		rl_redisplay();
-        env->put(env, "?", ft_strdup("130"));
-	}
-	// else if (sig == SIGSEGV)
-	//     ft_exit(0);
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 1);
+	rl_redisplay();
+	env->put(env, "?", ft_strdup("130"));
 }
 
 void	sig_handler(void)
